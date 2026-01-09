df45537063c348b76da584ec46cd03671db44852
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-01-09 22:10:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+#1-misol
+son = int(input("Son kiriting: "))
+if son % 2 == 0:
+    print("Juft")
+else:
+    print("Toq")
+
+#2-misol
+ball = int(input("Ball kiriting: "))
+if ball >= 90:
+    print("A'lo")
+elif ball >= 70:
+    print("Yaxshi")
+elif ball >= 50:
+    print("Qoniqarli")
+else:
+    print("Yiqildi")
+
+#3-misol
+son = int(input("Son kiriting: "))
+if son > 0:
+    print("Musbat")
+else:
+    print("Musbat emas")
+
+#4-misol
+a = int(input("a = "))
+b = int(input("b = "))
+if a > b:
+    print("Katta:", a)
+else:
+    print("Katta:", b)
+
+#5-misol
+son = int(input("Son kiriting: "))
+if son % 3 == 0:
+    print("3 ga bo‘linadi")
+else:
+    print("3 ga bo‘linmaydi")
